fd34f6a99b1c09410475420386c03e589b58ecd6
FabrizioMisseri <misserifabrizio@gmail.com>
2022-11-28 17:34:18 +0100

    scaffolding + markup

---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+// **Descrizione**
+// Dobbiamo creare una pagina che permetta ai nostri utenti di utilizzare il nostro generatore di password (abbastanza) sicure.
+// L’esercizio è suddiviso in varie milestone ed è molto importante svilupparle in modo ordinato.
+// **Milestone 1**
+// Creare un form che invii in GET la lunghezza della password. Una nostra funzione utilizzerà questo dato per generare una password casuale (composta da lettere, lettere maiuscole, numeri e simboli) da restituire all’utente.
+// Scriviamo tutto (logica e layout) in un unico file *index.php*
+
+// **Milestone 2**
+// Verificato il corretto funzionamento del nostro codice, spostiamo la logica in un file *functions.php* che includeremo poi nella pagina principale
+// **Milestone 3**
+// Invece di visualizzare la password nella index, effettuare un redirect ad una pagina dedicata che tramite $_SESSION recupererà la password da mostrare all’utente.
+// **Milestone 4 (BONUS)**
+// Gestire ulteriori parametri per la password: quali caratteri usare fra numeri, lettere e simboli. Possono essere scelti singolarmente (es. solo numeri) oppure possono essere combinati fra loro (es. numeri e simboli, oppure tutti e tre insieme).
+// Dare all’utente anche la possibilità di permettere o meno la ripetizione di caratteri uguali.
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<div class="my_wrapper">
+<div class="my_container">
+
+    <div class="container container-fluid">
+        <div class="mb-5">
+            <h1 class="text-center">Strong Password Generator</h1>
+            <h2 class="text-center">Genera una Password sicura</h2>
+        </div>
+
+        <!-- blocco parametro valido -->
+
+        <div class="row">
+            <div class="col-6">
+                <label for="pass">lunghezza password</label>
+            </div>
+            <div class="col-6">
+                <input type="number" id="pass">
+            </div>
+        </div>
+
+        <!-- spazio checkbox -->
+
+        <div class="row mt-5">
+            <div class="col-6">
+                <button class="btn btn-primary">INVIA</button>
+                <button class="btn btn-primary">RESET</button>
+            </div>
+        </div>
+    </div>
+
+</div>
+</div>
+</body>
+</html>
